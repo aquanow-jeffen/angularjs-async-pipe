48ab18fcfe73ee5cdf066b219903e4f8ac1ffa52
Jeffen Chen <jeffen.chen@central1.com>
2020-01-13 16:13:36 -0800

    feat: add example

--- example/app.js
+++ example/app.js
@@ -1,42 +1,27 @@
 const { interval } = rxjs;
-const { map, filter, tap } = rxjs.operators;
+const { map, startWith } = rxjs.operators;
 
 angular.module('app', [
   'asyncFilterModule'
 ])
-.component('heroDetail', {
-  template: `
-    <div>Name: {{$ctrl.hero | async:this }}</div>
-    <div>Name: {{$ctrl.hero1 | async:this }}</div>
-    <button ng-click="$ctrl.toggleMe()">Toggle Me</button>
-    <div ng-if="$ctrl.toggle1">Name: {{$ctrl.hero2 | async:this }}</div>
-  `,
-  bindings: {
-    hero: '=',
-    hero1: '=',
-    hero2: '='
-  }
-})
 .controller('MainCtrl', function MainCtrl() {
-  this.toggle1 = false;
+  this.toggle = false;
   this.toggleMe = () => {
-    this.toggle1 = !this.toggle1;
+    this.toggle = !this.toggle;
   }
-  this.hero = new Promise((res, rej) => {
-      res('This is a hero value');
+  this.promise = new Promise((res, rej) => {
+      res('Resoved immediately.');
   });
 
-  this.hero1 = new Promise((res, rej) => {
+  this.promiseWithTimeout = new Promise((res, rej) => {
     setTimeout(() => {
-      res('This is a hero1 value');
+      res('Resolved in 2000ms.');
     }, 2000);
   });
 
-  this.hero2 = interval(1000)
+  this.obs$ = interval(1000)
     .pipe(
-      tap(res => {
-        console.log(res)
-      }),
-      map(res => res*2)
+      startWith(0),
+      map(res => `${res*2}`)
     );
 });
